648428f1ebee5b1aae36266bc4378866184a1977
Brian Valiente Rodenas <103451491+brivaro@users.noreply.github.com>
2024-06-19 18:52:26 +0200

    Delete recursos_entrega directory

--- recursos_entrega/lib/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Answer"},{"p":"model","l":"Navegacion"},{"p":"DBAccess","l":"NavegacionDAO"},{"p":"DBAccess","l":"NavegacionDAOException"},{"p":"model","l":"Problem"},{"p":"model","l":"Session"},{"p":"DBAccess","l":"SqliteConnection"},{"p":"model","l":"User"}]
